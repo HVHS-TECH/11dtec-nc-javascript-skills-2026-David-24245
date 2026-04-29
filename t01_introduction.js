fc43fc3a90304dad8356679d2a967fe8ae4664ea
David-24245 <24245dl@hvhs.school.nz>
2026-04-29 02:19:06 +0000

    Apr 29, 2026, 2:19 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,19 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+Name of task: Introduction
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("Hello")
 
-// This is a single line comment
+// Variables
 
+
+/****************************
+Main code
+****************************/
+
+
+
+
+/****************************
+Functions
+****************************/
--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,22 @@
-/* Paste your code from task 1 here */
+/****************************
+Name of task: Variables
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Hello")
+
+// Variables
+let Username = "David";
+let year = 2026;
+let age = 15;
+let money = 20;
+
+/****************************
+Main code
+****************************/
+console.log("+ Username +" 
+
+
+
+/****************************
+Functions
+****************************/
