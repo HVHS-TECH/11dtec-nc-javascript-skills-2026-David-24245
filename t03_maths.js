f922170aa4418ee32d90d3e215c9eb44b809510d
David-24245 <24245dl@hvhs.school.nz>
2026-04-29 02:51:41 +0000

    Apr 29, 2026, 2:51 AM

--- t03_maths.js
+++ t03_maths.js
@@ -11,13 +11,14 @@ let pocketMoney = 20;
 let birthYear;
 let oldAge;
 birthYear = year - age;
-oldAge = age = + 18;
+oldAge = age = + 10;
 
 /****************************
 Main code
 ****************************/
 console.log("Hi "+ Username +" as of "+ Year +" you are "+ Age +" You have "+ pocketMoney +" dollars")
 console.log("You were born in " + birthYear)
+console.log("In 10 years you will be " + oldAge + "years old")
 
 
 /****************************
