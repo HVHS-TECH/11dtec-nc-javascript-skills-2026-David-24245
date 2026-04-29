e5795cd34a5dbeeeb1bf2ece35df2830e9b98721
David-24245 <24245dl@hvhs.school.nz>
2026-04-29 03:00:20 +0000

    Apr 29, 2026, 3:00 AM

--- t03_maths.js
+++ t03_maths.js
@@ -5,19 +5,19 @@ Name of task: Maths
 
 // Variables
 let Username = "David";
-let Year = 2026;
+let year = 2026;
 let Age = 15;
 let pocketMoney = 20;
 let birthYear;
 let oldAge;
-birthYear = Year - Age;
+birthYear = year - Age;
 oldAge = Age + 10;
 halfMoney = pocketMoney / 2;
 
 /****************************
 Main code
 ****************************/
-console.log("Hi "+ Username +" as of "+ Year +" you are "+ Age + " years old")
+console.log("Hi "+ Username +" as of "+ year +" you are "+ Age + " years old")
 console.log("You were born in " + birthYear)
 console.log("In 10 years you will be " + oldAge + " years old")
 console.log("You have " + pocketMoney + " dollars")
