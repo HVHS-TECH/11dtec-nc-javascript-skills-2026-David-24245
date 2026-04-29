18d4b98d3bce81355eb5523f81751c653a59ddb6
David-24245 <24245dl@hvhs.school.nz>
2026-04-29 02:26:56 +0000

    Apr 29, 2026, 2:26 AM

--- t02_variables.js
+++ t02_variables.js
@@ -13,7 +13,7 @@ let pocketMoney = 20;
 /****************************
 Main code
 ****************************/
-console.log("+ Username +", "+ Year +", "+ Age +", "You have" + pocketMoney + "dollars")
+console.log(+ Username +" as of "+ Year +" you are" + Age + "You have" + pocketMoney + "dollars")
 
 
 
