9383328391f5090bd6bd34998d1b24feafd86496
David-24245 <24245dl@hvhs.school.nz>
2026-04-29 02:53:42 +0000

    Apr 29, 2026, 2:53 AM

--- t03_maths.js
+++ t03_maths.js
@@ -19,6 +19,7 @@ Main code
 console.log("Hi "+ Username +" as of "+ Year +" you are "+ Age + "years old")
 console.log("You were born in " + birthYear)
 console.log("In 10 years you will be " + oldAge + "years old")
+console.log("You have " + money + " dollars")
 
 
 /****************************
