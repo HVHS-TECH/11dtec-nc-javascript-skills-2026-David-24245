a2f2bc00b04296cdadac407882cae12f37fb73aa
David-24245 <24245dl@hvhs.school.nz>
2026-04-29 02:43:47 +0000

    Apr 29, 2026, 2:43 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,21 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of task: Maths
+****************************/
+
+
+// Variables
+let Username = "David";
+let Year = 2026;
+let Age = 15;
+let pocketMoney = 20;
+
+/****************************
+Main code
+****************************/
+console.log("Hi "+ Username +" as of "+ Year +" you are "+ Age +" You have "+ pocketMoney +" dollars")
+
+
+
+/****************************
+Functions
+****************************/
