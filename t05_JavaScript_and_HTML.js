6a9ab3f3cbf2d8d08f71e4c5ca9e9790909ba8ff
David-24245 <24245dl@hvhs.school.nz>
2026-04-30 21:31:07 +0000

    Apr 30, 2026, 9:31 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,30 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Name of task: JavaScript And HTML
+****************************/
+
+
+// Variables
+let Username = "David";
+let year = 2025;
+let age = 15;
+let pocketMoney = 20;
+let birthYear;
+let oldAge;
+birthYear = year - age;
+oldAge = age + 10;
+halfMoney = pocketMoney / 2;
+
+/****************************
+Main code
+****************************/
+console.log("Hi "+ Username +" as of "+ year +" you are "+ age + " years old")
+console.log("You were born in " + birthYear)
+console.log("In 10 years you will be " + oldAge + " years old")
+console.log("You have " + pocketMoney + " dollars")
+console.log("You spend half of your money now you have " + halfMoney)
+console.log("Then you get $3, now you have " + (halfMoney + 3) + " dollars")
+
+
+/****************************
+Functions
+****************************/
