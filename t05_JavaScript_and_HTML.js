f631f81719fe2b7bb065225308d45dfcc847a061
David-24245 <24245dl@hvhs.school.nz>
2026-05-03 22:53:56 +0000

    May 3, 2026, 10:53 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -16,18 +16,18 @@ oldAge = age + 10;
 halfMoney = pocketMoney / 2;
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML = ("<h2>Added by JavaScript</h2>")
 
 /****************************
 Main code
 ****************************/
 
-OUTPUT.innerHTML += "<p>Hi "+ Username +" as of "+ year +" you are "+ age + " years old</p>";
-OUTPUT.innerHTML += "<p>You were born in " + birthYear "</p>";
-OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old</p>";
-OUTPUT.innerHTML += "<p>You have " + pocketMoney + " dollars</p>";
-OUTPUT.innerHTML += "<p>You spend half of your money now you have " + halfMoney</p>";
-OUTPUT.innerHTML += "<p>Then you get $3, now you have " + (halfMoney + 3) + " dollars</p>";
+OUTPUT.innerHTML += ("<p>Hi "+ Username +" as of "+ year +" you are "+ age + " years old</p>")
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear "</p>")
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old</p>")
+OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars</p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money now you have " + halfMoney</p>")
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + " dollars</p>")
 
 /****************************
 Functions
