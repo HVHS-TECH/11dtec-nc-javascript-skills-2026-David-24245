63287a2e5d63b2eaee52b53983fb1d9ec0aff045
David-24245 <24245dl@hvhs.school.nz>
2026-05-03 22:46:16 +0000

    May 3, 2026, 10:46 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -22,13 +22,6 @@ OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 Main code
 ****************************/
 
-console.log("Hi "+ Username +" as of "+ year +" you are "+ age + " years old")
-console.log("You were born in " + birthYear)
-console.log("In 10 years you will be " + oldAge + " years old")
-console.log("You have " + pocketMoney + " dollars")
-console.log("You spend half of your money now you have " + halfMoney)
-console.log("Then you get $3, now you have " + (halfMoney + 3) + " dollars")
-
 OUTPUT.innerHTML += "<p>"Hi "+ Username +" as of "+ year +" you are "+ age + " years old"</p>";
 OUTPUT.innerHTML += "<p>"You were born in " + birthYear</p>";
 OUTPUT.innerHTML += "<p>"In 10 years you will be " + oldAge + " years old"</p>";
