55e5821e9db3f4dc3edee9751dfd972be0fc10cf
David-24245 <24245dl@hvhs.school.nz>
2026-05-04 23:37:29 +0000

    May 4, 2026, 11:37 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,34 @@
-/* Paste your code from task 5 here */
+/****************************
+Name of task: JavaScript And HTML
+****************************/
+
+console.log("Running to5_JavaScript_and_HTML.js")
+
+// Variables
+let Username = "David";
+let year = 2026;
+let age = 15;
+let pocketMoney = 20;
+let birthYear;
+let oldAge;
+birthYear = year - age;
+oldAge = age + 10;
+halfMoney = pocketMoney / 2;
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+OUTPUT.innerHTML = ("<h2>Added by JavaScript</h2>")
+
+/****************************
+Main code
+****************************/
+
+OUTPUT.innerHTML += ("<p>Hi "+ Username +" as of "+ year +" you are "+ age + " years old</p>")
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear + " </p>")
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old</p>")
+OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars </p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money now you have " + halfMoney + " dollars</p>")
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + " dollars</p>")
+
+/****************************
+Functions
+****************************/
