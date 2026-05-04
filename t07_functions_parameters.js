58e2a67334f93c6b435e35212b72f54557ea4846
David-24245 <24245dl@hvhs.school.nz>
2026-05-04 23:54:50 +0000

    May 4, 2026, 11:54 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -21,8 +21,7 @@ OUTPUT.innerHTML = ("<h2>Added by JavaScript</h2>")
 /****************************
 Main code
 ****************************/
-Welcome()
-displayWelcome(_Username)
+displayWelcome(Username)
 
 OUTPUT.innerHTML += ("<p>Hi "+ Username +" as of "+ year +" you are "+ age + " years old</p>")
 OUTPUT.innerHTML += ("<p>You were born in " + birthYear + " </p>")
@@ -34,11 +33,6 @@ OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + " d
 /****************************
 Functions
 ****************************/
-
-function Welcome() {
-    OUTPUT.innerHTML += "<p> Welcome to the shop </p>";
-}
-
-function displayWelcome(_Username) {
-    OUTPUT.innerHTML += "<p> Welcome to the shop "+Username+"</p>";
+function displayWelcome(Username) {
+    OUTPUT.innerHTML += "<p> Welcome to the page "+Username+"</p>";
 }
