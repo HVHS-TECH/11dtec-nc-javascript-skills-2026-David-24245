aada9ff19fdc7fed9607ce74f20452c5896e9c35
David-24245 <24245dl@hvhs.school.nz>
2026-05-05 00:09:25 +0000

    May 5, 2026, 12:09 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -23,13 +23,6 @@ Main code
 ****************************/
 displayProduct("David", 15)
 
-OUTPUT.innerHTML += ("<p>Hi "+ Username +" as of "+ year +" you are "+ age + " years old</p>")
-OUTPUT.innerHTML += ("<p>You were born in " + birthYear + " </p>")
-OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old</p>")
-OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars </p>")
-OUTPUT.innerHTML += ("<p>You spend half of your money now you have " + halfMoney + " dollars</p>")
-OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + " dollars</p>")
-
 /****************************
 Functions
 ****************************/
