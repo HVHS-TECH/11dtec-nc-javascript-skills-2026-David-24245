60b91c40c778b4f5db9152c8a8c0b2a2ce51c33a
David-24245 <24245dl@hvhs.school.nz>
2026-05-07 21:26:09 +0000

    May 7, 2026, 9:26 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,40 @@
-/* Paste your code from task 7 here*/
+/****************************
+Name of task: Functions Parameters
+****************************/
+
+console.log("Running to7_functions_parameters.js")
+
+// Variables
+let Username = "";
+let year = 2026;
+let age = "";
+let item = "";
+let price = "";
+let pocketMoney = 20;
+let birthYear;
+let oldAge;
+birthYear = year - age;
+oldAge = age + 10;
+halfMoney = pocketMoney / 2;
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+OUTPUT.innerHTML = ("<h2>Added by JavaScript</h2>")
+
+/****************************
+Main code
+****************************/
+displayWelcome("David")
+displayProduct("Chocolate", 4)
+displayProduct("Chips", 3)
+displayProduct("Drink", 2.50)
+
+/****************************
+Functions
+****************************/
+function displayWelcome(Username) {
+    OUTPUT.innerHTML += "<p> Welcome to the shop "+Username+"</p>";
+}
+
+function displayProduct(item, price) {
+    OUTPUT.innerHTML += "<p> "+item+": $"+price+" </p>";
+}
