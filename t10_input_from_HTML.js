bfb2150f7d3455426b49a4274986d24f6c073378
David-24245 <24245dl@hvhs.school.nz>
2026-05-10 22:57:22 +0000

    May 10, 2026, 10:57 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -53,6 +53,6 @@ OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + " d
 
 <form onsubmit="return false;">
     <label for="formName">Name:</label>
-    <input type="text" id="nameField" name="formName">
+    <input type="text" id="nameField" name="formName"> placeHolder="name">
     <input type="submit" onClick=getFormInput()>
 </form>
