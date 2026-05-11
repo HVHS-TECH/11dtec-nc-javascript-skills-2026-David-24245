ba8a1550a4d0548fad82a08a7fd3c1e82538526f
David-24245 <24245dl@hvhs.school.nz>
2026-05-11 23:56:54 +0000

    May 11, 2026, 11:56 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -15,44 +15,22 @@ birthYear = year - age;
 oldAge = age + 10;
 halfMoney = pocketMoney / 2;
 
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
-OUTPUT.innerHTML = ("<h2>Added by JavaScript</h2>")
-
 function getFormInput(){
-const NAME_FIELD= document.getElementById("nameField");
-let userName = NAME_FIELD.value;
+const spaceForJavaScriptOutput= document.getElementById("spaceForJavaScriptOutput");
+let userName = spaceForJavaScriptOutput.value;
 OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
 }
+
 /****************************
 Main code
 ****************************/
-Welcome()
-start()
 
 /****************************
 Functions
 ****************************/
 
-function Welcome() {
-OUTPUT.innerHTML += "<p> Welcome to the shop "+Username+"</p>";
-}
-
-function start() {
-    spaceForJavaScriptOutput.innerHTML
-    spaceForJavaScriptOutput.innerHTML
-    answer = year - age;
-    spaceForJavaScriptOutput.innerHTML
-
-OUTPUT.innerHTML += ("<p>Hi "+ Username +" as of "+ year +" you are "+ age + " years old</p>")
-OUTPUT.innerHTML += ("<p>You were born in " + birthYear + " </p>")
-OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old</p>")
-OUTPUT.innerHTML += ("<p>You have " + pocketMoney + " dollars </p>")
-OUTPUT.innerHTML += ("<p>You spend half of your money now you have " + halfMoney + " dollars</p>")
-OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + (halfMoney + 3) + " dollars</p>")
-}
-
 <form onsubmit="return false;">
     <label for="formName">Name:</label>
     <input type="text" id="nameField" name="formName" placeHolder="name">
-    <input type="submit" onClick=getFormInput()></input>
+    <input type="submit" onClick=getFormInput()>
 </form>
