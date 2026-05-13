f21bce62027deedd4f59603078a7561ab694b179
David-24245 <24245dl@hvhs.school.nz>
2026-05-13 02:59:05 +0000

    May 13, 2026, 2:59 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -31,7 +31,7 @@ let userName = NAME_FIELD.value;
 OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
 }
 
-<form onsubmit="return false;"
+<form onsubmit="return false;">
     <label for="formName">Name:</label>
     <input type="text" id="spaceForJavaScriptOutput" name="formName" placeHolder="name"></input>
     <input type="submit" onClick="getFormInput"></input>
