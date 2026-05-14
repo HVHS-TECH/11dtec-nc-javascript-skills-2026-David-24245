17c37accba5e7920c0d7c3bf021762201d09de72
David-24245 <24245dl@hvhs.school.nz>
2026-05-14 21:16:30 +0000

    May 14, 2026, 9:16 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,8 +1,31 @@
-/* Paste your code from the last task */
-/* 
-<form onsubmit="return false;">
-    <label for="formName">Name:</label>
-    <input type="text" id="spaceForJavaScriptOutput" name="formName" placeHolder="name"></input>
-    <input type="submit" onClick=getFormInput()>
-</form> 
-*/
+/****************************
+Name of task: Data Types
+****************************/
+
+console.log("Running t11_data_types.js")
+
+// Variables
+let userName = "David";
+let year = 2026;
+let age = 15;
+let pocketMoney = 20;
+let birthYear;
+let oldAge;
+let halfMoney;        
+birthYear = year - age;
+oldAge = age + 10;
+halfMoney = pocketMoney / 2;
+
+/****************************
+Main code
+****************************/
+
+/****************************
+Functions
+****************************/
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");   
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");  
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>";
+}
