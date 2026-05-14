ab762f3ac420f7c465d7f71dd04100297807f7a4
David-24245 <24245dl@hvhs.school.nz>
2026-05-14 11:13:12 +0000

    May 14, 2026, 11:13 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -5,34 +5,27 @@ Name of task: Input From HTML
 console.log("Running t10_input_from_HTML.js")
 
 // Variables
-let Username = "David";
 let userName = "David";
 let year = 2026;
 let age = 15;
 let pocketMoney = 20;
 let birthYear;
 let oldAge;
+let halfMoney;        
 birthYear = year - age;
 oldAge = age + 10;
 halfMoney = pocketMoney / 2;
 
-
 /****************************
 Main code
 ****************************/
-getFormInput()
 
 /****************************
 Functions
 ****************************/
 function getFormInput(){
-const NAME_FIELD= document.getElementById("spaceForJavaScriptOutput");
-let userName = NAME_FIELD.value;
-OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+    const NAME_FIELD = document.getElementById("nameField");   
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");  
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>";
 }
-
-<form onsubmit="return false;">
-    <label for="formName">Name:</label>
-    <input type="text" id="spaceForJavaScriptOutput" name="formName" placeHolder="name"></input>
-    <input type="submit" onClick="getFormInput"></input>
-</form>
