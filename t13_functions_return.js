70268a7504274e1b312b12b25978bcd97d035c18
David-24245 <24245dl@hvhs.school.nz>
2026-05-24 22:43:31 +0000

    May 24, 2026, 10:43 PM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,41 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Functions Return
+****************************/
+
+console.log("Running t13_functions_return.js")
+
+// Variables
+let userName = "David";
+let year = 2026;
+let age = 15;
+let pocketMoney = 20;
+let birthYear;
+let oldAge;
+let halfMoney;        
+birthYear = year - age;
+oldAge = age + 10;
+halfMoney = pocketMoney / 2;
+
+/****************************
+Main code
+****************************/
+
+/****************************
+Functions
+****************************/
+function getFormInput(){
+    const MONEY_FIELD = document.getElementById("moneyField");   
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");  
+    let userMoney = MONEY_FIELD.value;
+
+    const AGE_FIELD = document.getElementById("ageField");   
+    let userAge = AGE_FIELD.value;
+
+    OUTPUT.innerHTML = "<p>You are " + userAge + " years old and you have $" + userMoney +"</p>";
+
+        if (userMoney < 4){
+    console.log("Sorry you can't afford a chocolate bar")
+} else if (userMoney >= 4){
+    console.log("You can afford a chocolate bar")
+}
+}
