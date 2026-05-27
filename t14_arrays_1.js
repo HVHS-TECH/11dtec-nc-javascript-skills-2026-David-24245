a4d6fab7ff708cda9190274ac3e56c413dee16ae
David-24245 <24245dl@hvhs.school.nz>
2026-05-27 02:20:11 +0000

    May 27, 2026, 2:20 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -1,1 +1,40 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Return Values
+****************************/
+
+console.log("Running t13_functions_return.js")
+
+// Variables
+let userName = "David";
+let year = 2026;
+let age = 15;
+let pocketMoney = 20;
+let birthYear;
+let oldAge;
+let halfMoney;        
+birthYear = year - age;
+oldAge = age + 10;
+halfMoney = pocketMoney / 2;
+
+/****************************
+Functions
+****************************/
+function calculateChange(_money, _price){
+    let change = _money - _price;
+    return change;
+}
+
+function getFormInput(){
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    let userMoney = document.getElementById("moneyField").value;
+
+    if (userMoney < 4){
+        console.log("Sorry you can't afford a chocolate bar");
+        OUTPUT.innerHTML = "<p>Sorry, you can't afford a chocolate bar.</p>";
+    } else {
+        let change = calculateChange(userMoney, 4);
+        console.log("You can afford a chocolate bar");
+        OUTPUT.innerHTML = "<p>You can afford a chocolate bar!</p>";
+        OUTPUT.innerHTML += "<p>You will get $" + change + " change.</p>";
+    }
+}
