3fc0cbdece331bceaf90128e9bb353c9628b6b02
David-24245 <24245dl@hvhs.school.nz>
2026-06-01 03:44:20 +0000

    Jun 1, 2026, 3:44 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -8,33 +8,15 @@ function getFormInput() {
     const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
     let startNumber = parseInt(document.getElementById("versesField").value);
 
-    if (isNaN(startNumber)) {
-        OUTPUT.innerHTML = "<p>Please enter a number between 1 and 99.</p>";
-        return;
-    }
-    if (startNumber < 1) {
-        OUTPUT.innerHTML = "<p>Please enter a number between 1 and 99.</p>";
-        return;
-    }
-    if (startNumber > 99) {
-        OUTPUT.innerHTML = "<p>Please enter a number between 1 and 99.</p>";
-        return;
-    }
-
     let lyrics = "";
 
     for (let i = startNumber; i >= 1; i--) {
-        let next = i - 1;
-        let nextWord = next === 1 ? "1 bottle" : next + " bottles";
-        lyrics += "<p>" + i + " bottle" + (i === 1 ? "" : "s") + " of milk on the wall, "
-               + i + " bottle" + (i === 1 ? "" : "s") + " of milk.<br>"
-               + "Take one down, pass it around, "
-               + (next === 0 ? "no more bottles" : nextWord)
-               + " of milk on the wall.</p>";
+        lyrics += "<p>" + i + " bottles of milk on the wall, " + i + " bottles of milk.<br>"
+               + "Take one down, pass it around, " + (i - 1) + " bottles of milk on the wall.</p>";
+
+        console.log("Current count: " + i);
     }
 
-    lyrics += "<p>No more bottles of milk on the wall, no more bottles of milk.<br>"
-            + "Go to the store and buy some more, 99 bottles of milk on the wall.</p>";
-
     OUTPUT.innerHTML = lyrics;
+    console.log("Loop stopped");
 }
