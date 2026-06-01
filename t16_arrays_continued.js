1aa243573ff80385fed9c50324cc2fd99f27d39a
David-24245 <24245dl@hvhs.school.nz>
2026-06-01 03:49:06 +0000

    Jun 1, 2026, 3:49 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,34 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Arrays 2
+****************************/
+
+console.log("Running t16_arrays_2.js")
+
+// Variables
+let shoppingList = [];
+
+/****************************
+Functions
+****************************/
+function addItem() {
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    let item = document.getElementById("itemField").value;
+
+    shoppingList.push(item);
+    console.log("Added: " + item);
+    OUTPUT.innerHTML = "<p>You have added " + item + " to the list.</p>";
+
+    document.getElementById("itemField").value = "";
+}
+
+function displayList() {
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+    OUTPUT.innerHTML = "<p>These are the items on your shopping list:</p>";
+
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += "Person " + i + ": " + shoppingList[i] + "<br>";
+    }
+
+    console.log("List displayed. Items: " + shoppingList.length);
+}
