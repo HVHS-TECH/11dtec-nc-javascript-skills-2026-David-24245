0f74230e0fad1839209c421b2d0b764b491174cc
David-24245 <24245dl@hvhs.school.nz>
2026-06-03 02:37:09 +0000

    Jun 3, 2026, 2:37 AM

--- t18_validation.js
+++ t18_validation.js
@@ -27,15 +27,14 @@ function addItem() {
     console.log("Added: " + item);
     OUTPUT.innerHTML = "<p>You have added " + item + " to the list.</p>";
     document.getElementById("itemField").value = "";  
-    }
+}
 
 
 function displayList() {
     const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
     OUTPUT.innerHTML = "<p>These are the items on your shopping list:</p>";
     for (let i = 0; i < shoppingList.length; i++) {
-        OUTPUT.innerHTML += shoppingList[i] + "<br>";
+    OUTPUT.innerHTML += shoppingList[i] + "<br>";
     }
     console.log("List displayed Items: " + shoppingList.length);
 }
-
