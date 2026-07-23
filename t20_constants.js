14f38754e279025515da39d23a775a62ed196aa3
David-24245 <24245dl@hvhs.school.nz>
2026-07-23 09:52:33 +0000

    Jul 23, 2026, 9:52 AM

--- t20_constants.js
+++ t20_constants.js
@@ -1,1 +1,57 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Constants and Conventions
+****************************/
+
+console.log("Running t20_constants_and_conventions.js")
+
+const USER_NAME = "David";
+const YEAR = 2026;
+const AGE = 15;
+const POCKET_MONEY = 20;
+const BIRTH_YEAR = YEAR - AGE;
+const OLD_AGE = AGE + 10;
+const HALF_MONEY = POCKET_MONEY / 2;
+const MIN_NAME_LENGTH = 3;
+
+// Variables
+let shoppingList = [];
+
+/****************************
+Functions
+****************************/
+function addItem() {
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    const item = document.getElementById("itemField").value;
+    shoppingList.push(item);
+    console.log("Added: " + item);
+    OUTPUT.innerHTML = "<p>You have added " + item + " to the list.</p>";
+    document.getElementById("itemField").value = "";
+}
+
+
+function displayList() {
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    OUTPUT.innerHTML = "<p>These are the items on your shopping list:</p>";
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += shoppingList[i] + "<br>";
+    }
+    console.log("List displayed Items: " + shoppingList.length);
+}
+
+
+function checkName() {
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    let name = document.getElementById("nameField").value;
+
+    name = name.trim();
+
+    if (name.length >= MIN_NAME_LENGTH) {
+        OUTPUT.innerHTML = "<p>Thanks, " + name + "! That's a valid name.</p>";
+        console.log("Valid name entered: " + name);
+    } else {
+        OUTPUT.innerHTML = "<p>Please enter a name that is at least " + MIN_NAME_LENGTH + " letters long.</p>";
+        console.log("Invalid name entered: " + name);
+    }
+
+    document.getElementById("nameField").value = "";
+}
